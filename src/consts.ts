da472f66dfaa8c5887273de273f06a3c724ee17f
Md. Nazmul Huda <me@nazmul-huda.com>
2025-05-03 16:00:17 +0600

    version 2.0

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'মোঃ নাজমূল হুদা'
+export const SITE_DESCRIPTION = 'আমি একজন বিজনেস অ্যানালিস্ট!'
